af3b5d6e857c19962a136b27275fc7e9a449f3ed
nikola.stojkovic <nikola.stojkovic@intellya.ai>
2022-04-25 17:16:11 +0200

    add health check endpoints for mvc web app

--- src/WebApps/AspnetRunBasics/Startup.cs
+++ src/WebApps/AspnetRunBasics/Startup.cs
@@ -2,10 +2,13 @@ using System;
 using System.Net.Http;
 using AspnetRunBasics.Services;
 using Common.Logging;
+using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
 using Polly;
 using Polly.Extensions.Http;
@@ -48,6 +51,12 @@ namespace AspnetRunBasics
 
 
             services.AddRazorPages();
+
+            services.AddHealthChecks()
+              .AddUrlGroup(
+                  new Uri($"{Configuration["ApiSettings:GatewayUrl"]}/hc"),
+                  "Catalog.API",
+                  HealthStatus.Degraded);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -74,6 +83,11 @@ namespace AspnetRunBasics
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
+                endpoints.MapHealthChecks("/hc", new HealthCheckOptions()
+                {
+                    Predicate = _ => true,
+                    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+                });
             });
         }
 
