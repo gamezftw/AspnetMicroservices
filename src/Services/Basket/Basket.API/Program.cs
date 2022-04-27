cb1ccba1ac0f30a4ecf755062661d86e7cc52692
nikola.stojkovic <nikola.stojkovic@intellya.ai>
2022-04-27 09:36:14 +0200

    add OpenTelemetry stuff to basket api

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -7,10 +7,51 @@ using Common.Logging;
 using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Trace;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Resources;
+using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Host.UseSerilog(SeriLogger.Configure);
+
+
+// Configure metrics
+builder.Services.AddOpenTelemetryMetrics(otelBuilder =>
+{
+    otelBuilder.AddHttpClientInstrumentation();
+    otelBuilder.AddAspNetCoreInstrumentation();
+    otelBuilder.AddMeter("MyApplicationMetrics");
+    otelBuilder.SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(builder.Configuration["OpenTelemetry:ResourceName"]));
+    otelBuilder.AddOtlpExporter(options => options.Endpoint = new Uri(builder.Configuration["OpenTelemetry:CollectorUrl"]));
+});
+
+// Configure tracing
+builder.Services.AddOpenTelemetryTracing(otelBuilder =>
+{
+    otelBuilder.AddHttpClientInstrumentation();
+    otelBuilder.AddGrpcClientInstrumentation();
+    otelBuilder.AddAspNetCoreInstrumentation();
+    otelBuilder.AddMassTransitInstrumentation();
+    otelBuilder.Configure((provider, b) => {
+        var connectionMultiplexer = provider.GetRequiredService<IConnectionMultiplexer>();
+        b.AddRedisInstrumentation(connectionMultiplexer);
+    });
+    otelBuilder.AddSource("MyApplicationActivitySource");
+    otelBuilder.SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(builder.Configuration["OpenTelemetry:ResourceName"]));
+    otelBuilder.AddOtlpExporter(options => options.Endpoint = new Uri(builder.Configuration["OpenTelemetry:CollectorUrl"]));
+});
+
+// Configure logging
+builder.Logging.AddOpenTelemetry(otelBuilder =>
+{
+    otelBuilder.IncludeFormattedMessage = true;
+    otelBuilder.IncludeScopes = true;
+    otelBuilder.ParseStateValues = true;
+    otelBuilder.SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(builder.Configuration["OpenTelemetry:ResourceName"]));
+});
 
 // Add services to the container.
 
---
+++ src/Services/Basket/Basket.API/README.md
@@ -0,0 +1,3 @@
+# Open telemetry
+- https://github.com/open-telemetry/opentelemetry-dotnet/blob/main/src/OpenTelemetry.Instrumentation.GrpcNetClient/README.md
+- https://www.nuget.org/packages/OpenTelemetry.Contrib.Instrumentation.MassTransit/1.0.0-beta2
