39309402c2445136a345c0a00ca20c44b7ab2963
nikola.stojkovic <nikola.stojkovic@intellya.ai>
2022-04-27 10:01:02 +0200

    add OpenTelemetry stuff to discount grpc

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -5,10 +5,45 @@ using Serilog;
 using Common.Logging;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using HealthChecks.UI.Client;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using OpenTelemetry.Logs;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Host.UseSerilog(SeriLogger.Configure);
+
+// Configure metrics
+builder.Services.AddOpenTelemetryMetrics(otelBuilder =>
+{
+    otelBuilder.AddHttpClientInstrumentation();
+    otelBuilder.AddAspNetCoreInstrumentation();
+    otelBuilder.AddMeter("MyApplicationMetrics");
+    otelBuilder.SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(builder.Configuration["OpenTelemetry:ResourceName"]));
+    otelBuilder.AddOtlpExporter(options => options.Endpoint = new Uri(builder.Configuration["OpenTelemetry:CollectorUrl"]));
+});
+
+// Configure tracing
+builder.Services.AddOpenTelemetryTracing(otelBuilder =>
+{
+    otelBuilder.AddHttpClientInstrumentation();
+    otelBuilder.AddAspNetCoreInstrumentation();
+    otelBuilder.AddMassTransitInstrumentation();
+    otelBuilder.AddSqlClientInstrumentation(); // TODO this uses SqlClient instrumentation, find out what is Npgsql.OpenTelemetry
+    otelBuilder.AddSource("MyApplicationActivitySource");
+    otelBuilder.SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(builder.Configuration["OpenTelemetry:ResourceName"]));
+    otelBuilder.AddOtlpExporter(options => options.Endpoint = new Uri(builder.Configuration["OpenTelemetry:CollectorUrl"]));
+});
+
+// Configure logging
+builder.Logging.AddOpenTelemetry(otelBuilder =>
+{
+    otelBuilder.IncludeFormattedMessage = true;
+    otelBuilder.IncludeScopes = true;
+    otelBuilder.ParseStateValues = true;
+    otelBuilder.AddOtlpExporter(options => options.Endpoint = new Uri(builder.Configuration["OpenTelemetry:CollectorUrl"]));
+});
 
 // Additional configuration is required to successfully run gRPC on macOS.
 // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
