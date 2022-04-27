6208ae1840e261a754e539c26d6c2f80e8179dc0
nikola.stojkovic <nikola.stojkovic@intellya.ai>
2022-04-27 10:26:32 +0200

    add OpenTelemetry and healthcehck stuff to ocelot gateway

--- src/ApiGateways/OcelotApiGw/Program.cs
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -3,8 +3,44 @@ using Ocelot.Cache.CacheManager;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 using Serilog;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using HealthChecks.UI.Client;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Trace;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Resources;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Configure metrics
+builder.Services.AddOpenTelemetryMetrics(otelBuilder =>
+{
+    otelBuilder.AddHttpClientInstrumentation();
+    otelBuilder.AddAspNetCoreInstrumentation();
+    otelBuilder.AddMeter("MyApplicationMetrics");
+    otelBuilder.SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(builder.Configuration["OpenTelemetry:ResourceName"]));
+    otelBuilder.AddOtlpExporter(options => options.Endpoint = new Uri(builder.Configuration["OpenTelemetry:CollectorUrl"]));
+});
+
+// Configure tracing
+builder.Services.AddOpenTelemetryTracing(otelBuilder =>
+{
+    otelBuilder.AddHttpClientInstrumentation();
+    otelBuilder.AddAspNetCoreInstrumentation();
+    otelBuilder.AddSource("MyApplicationActivitySource");
+    otelBuilder.SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(builder.Configuration["OpenTelemetry:ResourceName"]));
+    otelBuilder.AddOtlpExporter(options => options.Endpoint = new Uri(builder.Configuration["OpenTelemetry:CollectorUrl"]));
+});
+
+// Configure logging
+builder.Logging.AddOpenTelemetry(otelBuilder =>
+{
+    otelBuilder.IncludeFormattedMessage = true;
+    otelBuilder.IncludeScopes = true;
+    otelBuilder.ParseStateValues = true;
+    otelBuilder.SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(builder.Configuration["OpenTelemetry:ResourceName"]));
+});
 
 builder.Host.UseSerilog(SeriLogger.Configure);
 
@@ -15,6 +51,13 @@ builder.Services.AddOcelot()
 
 var app = builder.Build();
 
+app.MapHealthChecks("/hc", new HealthCheckOptions()
+    {
+      Predicate = _ => true,
+      ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
+
+app.Run();
 
 app.MapGet("/", () => "Hello World!");
 
