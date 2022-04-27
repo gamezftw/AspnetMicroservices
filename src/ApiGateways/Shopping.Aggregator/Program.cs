c1d5acf136b90cf80cf96636f2f176534f16e80a
nikola.stojkovic <nikola.stojkovic@intellya.ai>
2022-04-27 09:06:52 +0200

    add OpenTelemetry stuff to shopping aggregator

--- src/ApiGateways/Shopping.Aggregator/Program.cs
+++ src/ApiGateways/Shopping.Aggregator/Program.cs
@@ -6,10 +6,43 @@ using Polly.Extensions.Http;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using HealthChecks.UI.Client;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Trace;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Resources;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Host.UseSerilog(SeriLogger.Configure);
+
+// Configure metrics
+builder.Services.AddOpenTelemetryMetrics(otelBuilder =>
+{
+    otelBuilder.AddHttpClientInstrumentation();
+    otelBuilder.AddAspNetCoreInstrumentation();
+    otelBuilder.AddMeter("MyApplicationMetrics");
+    otelBuilder.SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(builder.Configuration["OpenTelemetry:ResourceName"]));
+    otelBuilder.AddOtlpExporter(options => options.Endpoint = new Uri(builder.Configuration["OpenTelemetry:CollectorUrl"]));
+});
+
+// Configure tracing
+builder.Services.AddOpenTelemetryTracing(otelBuilder =>
+{
+    otelBuilder.AddHttpClientInstrumentation();
+    otelBuilder.AddAspNetCoreInstrumentation();
+    otelBuilder.AddSource("MyApplicationActivitySource");
+    otelBuilder.SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(builder.Configuration["OpenTelemetry:ResourceName"]));
+    otelBuilder.AddOtlpExporter(options => options.Endpoint = new Uri(builder.Configuration["OpenTelemetry:CollectorUrl"]));
+});
+
+// Configure logging
+builder.Logging.AddOpenTelemetry(otelBuilder =>
+{
+    otelBuilder.IncludeFormattedMessage = true;
+    otelBuilder.IncludeScopes = true;
+    otelBuilder.ParseStateValues = true;
+    otelBuilder.SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(builder.Configuration["OpenTelemetry:ResourceName"]));
+});
 
 // Add services to the container.
 
